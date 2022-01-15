35c12b1260d8d64cc4ed4d3d0e936149cdf0fdb6
Dayton Bobbitt <12210193+dayton-bobbitt@users.noreply.github.com>
2022-01-15 14:56:45 -0600

    Setup animations for player movement

--- Player/Player.gd
+++ Player/Player.gd
@@ -2,17 +2,22 @@ extends KinematicBody2D
 
 
 export var max_speed: float = 75
-export var acceleration: float = 10
-export var friction: float = 10
+export var acceleration: float = 15
+export var friction: float = 15
 
 var _velocity = Vector2.ZERO
+
+onready var _animation_tree = $AnimationTree
+onready var _animation_tree_playback = _animation_tree.get("parameters/playback")
 
 
 func _physics_process(delta):
 	var direction = _get_direction()
 	
 	_update_velocity(direction)
-	move_and_slide(_velocity)
+	_update_animation(direction)
+	
+	_velocity = move_and_slide(_velocity)
 
 
 # Returns a normalized vector based on player input
@@ -25,10 +30,26 @@ func _get_direction() -> Vector2:
 	return direction.normalized()
 
 
+# Returns a boolean based on the direction of player input
+func _is_moving(direction: Vector2) -> bool:
+	return direction != Vector2.ZERO
+
+
 func _update_velocity(direction: Vector2) -> void:
-	if direction == Vector2.ZERO:
-		# player is NOT moving; reduce velocity by friction until zero
-		_velocity = _velocity.move_toward(Vector2.ZERO, friction)
-	else:
-		# player is moving; increase velocity by acceleration until max_speed
+	if _is_moving(direction):
+		# increase velocity by acceleration until max_speed
 		_velocity = _velocity.move_toward(direction * max_speed, acceleration)
+	else:
+		# reduce velocity by friction until zero
+		_velocity = _velocity.move_toward(Vector2.ZERO, friction)
+
+
+func _update_animation(direction: Vector2) -> void:
+	if _is_moving(direction):
+		_animation_tree.set("parameters/Idle/blend_position", direction)
+		_animation_tree.set("parameters/Move/blend_position", direction)
+		_animation_tree_playback.travel("Move")
+	else:
+		_animation_tree_playback.travel("Idle")
+
+
