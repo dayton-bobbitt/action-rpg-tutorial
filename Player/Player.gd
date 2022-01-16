0820bd14d0a0ee3a505adc50ee2db384add1029c
Dayton Bobbitt <12210193+dayton-bobbitt@users.noreply.github.com>
2022-01-16 08:24:32 -0600

    Utilize state machine

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,72 +1,118 @@
 extends KinematicBody2D
 
 
+enum State {
+	IDLE,
+	MOVE,
+	ATTACK
+}
+
+const ANIMATION = {
+	IDLE = "Idle",
+	MOVE = "Move",
+	ATTACK = "Attack"
+}
+const ANIMATION_BLEND_POSITION_PARAM = "parameters/%s/blend_position"
+const INPUT = {
+	LEFT = "ui_left",
+	RIGHT = "ui_right",
+	UP ="ui_up",
+	DOWN = "ui_down",
+	ATTACK = "ui_attack"
+}
+const MAX_SPEED: int = 75
+const ACCELERATION: int = 15
 const DEFAULT_FRICTION: int = 15
-const ATTACK_FRICTION: int = 4
+const ATTACK_FRICTION: int = 5
 
-export var max_speed: float = 75
-export var acceleration: float = 15
-export var friction: float = DEFAULT_FRICTION
-
-var _is_attacking = false
+var _state = State.IDLE
 var _velocity = Vector2.ZERO
 
 onready var _animation_tree = $AnimationTree
 onready var _animation_tree_playback = _animation_tree.get("parameters/playback")
 
 
+func _ready():
+	_animation_tree.active = true
+
+
 func _physics_process(_delta):
 	var direction = _get_direction()
+	var is_moving = direction != Vector2.ZERO
+	var is_attacking = _get_is_attacking()
 	
-	_update_is_attacking()
-	_update_velocity(direction)
-	_update_animation(direction)
+	_update_state(is_attacking, is_moving)
 	
-	_velocity = move_and_slide(_velocity)
+	match _state:
+		State.IDLE:
+			_player_idle()
+		State.MOVE:
+			_player_move(direction)
+		State.ATTACK:
+			_player_attack()
 
 
 # Returns a normalized vector based on player input
 func _get_direction() -> Vector2:
 	var direction = Vector2.ZERO
 	
-	direction.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	direction.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	direction.x = Input.get_action_strength(INPUT.RIGHT) - Input.get_action_strength(INPUT.LEFT)
+	direction.y = Input.get_action_strength(INPUT.DOWN) - Input.get_action_strength(INPUT.UP)
 	
 	return direction.normalized()
 
 
-func _update_is_attacking() -> void:
-	if not _is_attacking and Input.is_action_just_pressed("ui_attack"):
-		_is_attacking = true
-		friction = ATTACK_FRICTION
-
-
-# Returns a boolean based on the direction of player input
-func _is_moving(direction: Vector2) -> bool:
-	return direction != Vector2.ZERO
-
-
-func _update_velocity(direction: Vector2) -> void:
-	if _is_attacking or not _is_moving(direction):
-		# reduce velocity by friction until zero
-		_velocity = _velocity.move_toward(Vector2.ZERO, friction)
+func _get_is_attacking() -> bool:
+	var is_currently_attacking = _state == State.ATTACK
+	
+	if is_currently_attacking:
+		return true
 	else:
-		# increase velocity by acceleration until max_speed
-		_velocity = _velocity.move_toward(direction * max_speed, acceleration)
+		return Input.is_action_just_pressed(INPUT.ATTACK)
 
 
-func _update_animation(direction: Vector2) -> void:
-	if _is_attacking:
-		_animation_tree_playback.travel("Attack")
-	elif _is_moving(direction):
-		_animation_tree.set("parameters/Attack/blend_position", direction)
-		_animation_tree.set("parameters/Idle/blend_position", direction)
-		_animation_tree.set("parameters/Move/blend_position", direction)
-		_animation_tree_playback.travel("Move")
+func _update_state(is_attacking: bool, is_moving: bool) -> void:
+	if is_attacking:
+		_state = State.ATTACK
+	elif is_moving:
+		_state = State.MOVE
 	else:
-		_animation_tree_playback.travel("Idle")
+		_state = State.IDLE
+
+
+func _player_idle() -> void:
+	_stop_moving()
+	_animation_tree_playback.travel(ANIMATION.IDLE)
+
+
+func _player_move(direction: Vector2) -> void:
+	_update_animation_blend_position(direction)
+	_move(direction)
+	_animation_tree_playback.travel(ANIMATION.MOVE)
+
+
+func _player_attack() -> void:
+	_stop_moving(ATTACK_FRICTION)
+	_animation_tree_playback.travel(ANIMATION.ATTACK)
+
+
+# Increase velocity by ACCELERATION until MAX_SPEED
+func _move(direction: Vector2) -> void:
+	var new_velocity = _velocity.move_toward(direction * MAX_SPEED, ACCELERATION)
+	_velocity = move_and_slide(new_velocity)
+
+
+# Reduce velocity by friction until zero
+func _stop_moving(friction := DEFAULT_FRICTION) -> void:
+	var new_velocity = _velocity.move_toward(Vector2.ZERO, friction)
+	_velocity = move_and_slide(new_velocity)
+
+
+func _update_animation_blend_position(blend_position: Vector2) -> void:
+	_animation_tree.set(ANIMATION_BLEND_POSITION_PARAM % ANIMATION.IDLE, blend_position)
+	_animation_tree.set(ANIMATION_BLEND_POSITION_PARAM % ANIMATION.MOVE, blend_position)
+	_animation_tree.set(ANIMATION_BLEND_POSITION_PARAM % ANIMATION.ATTACK, blend_position)
 
 
 func _attack_animation_finished() -> void:
-	_is_attacking = false
-	friction = DEFAULT_FRICTION
+	_state = State.IDLE
