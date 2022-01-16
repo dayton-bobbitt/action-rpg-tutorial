69fc7c05da2753f1102894a835022faba0aa5aac
Dayton Bobbitt <12210193+dayton-bobbitt@users.noreply.github.com>
2022-01-15 22:29:51 -0600

    Setup attack animations

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,19 +1,24 @@
 extends KinematicBody2D
 
 
+const DEFAULT_FRICTION: int = 15
+const ATTACK_FRICTION: int = 4
+
 export var max_speed: float = 75
 export var acceleration: float = 15
-export var friction: float = 15
+export var friction: float = DEFAULT_FRICTION
 
+var _is_attacking = false
 var _velocity = Vector2.ZERO
 
 onready var _animation_tree = $AnimationTree
 onready var _animation_tree_playback = _animation_tree.get("parameters/playback")
 
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	var direction = _get_direction()
 	
+	_update_is_attacking()
 	_update_velocity(direction)
 	_update_animation(direction)
 	
@@ -30,22 +35,31 @@ func _get_direction() -> Vector2:
 	return direction.normalized()
 
 
+func _update_is_attacking() -> void:
+	if not _is_attacking and Input.is_action_just_pressed("ui_attack"):
+		_is_attacking = true
+		friction = ATTACK_FRICTION
+
+
 # Returns a boolean based on the direction of player input
 func _is_moving(direction: Vector2) -> bool:
 	return direction != Vector2.ZERO
 
 
 func _update_velocity(direction: Vector2) -> void:
-	if _is_moving(direction):
-		# increase velocity by acceleration until max_speed
-		_velocity = _velocity.move_toward(direction * max_speed, acceleration)
-	else:
+	if _is_attacking or not _is_moving(direction):
 		# reduce velocity by friction until zero
 		_velocity = _velocity.move_toward(Vector2.ZERO, friction)
+	else:
+		# increase velocity by acceleration until max_speed
+		_velocity = _velocity.move_toward(direction * max_speed, acceleration)
 
 
 func _update_animation(direction: Vector2) -> void:
-	if _is_moving(direction):
+	if _is_attacking:
+		_animation_tree_playback.travel("Attack")
+	elif _is_moving(direction):
+		_animation_tree.set("parameters/Attack/blend_position", direction)
 		_animation_tree.set("parameters/Idle/blend_position", direction)
 		_animation_tree.set("parameters/Move/blend_position", direction)
 		_animation_tree_playback.travel("Move")
@@ -53,3 +67,6 @@ func _update_animation(direction: Vector2) -> void:
 		_animation_tree_playback.travel("Idle")
 
 
+func _attack_animation_finished() -> void:
+	_is_attacking = false
+	friction = DEFAULT_FRICTION
