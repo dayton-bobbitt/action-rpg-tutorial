91d2ba9bbecbb178d2ebaa24bacbbdbbbff896f2
Dayton Bobbitt <12210193+dayton-bobbitt@users.noreply.github.com>
2022-01-16 16:35:59 -0600

    Add ability to roll and attack cooldowns

--- Player/Player.gd
+++ Player/Player.gd
@@ -4,12 +4,14 @@ extends KinematicBody2D
 enum State {
 	IDLE,
 	MOVE,
+	ROLL,
 	ATTACK
 }
 
 const ANIMATION = {
 	IDLE = "Idle",
 	MOVE = "Move",
+	ROLL = "Roll",
 	ATTACK = "Attack"
 }
 const ANIMATION_BLEND_POSITION_PARAM = "parameters/%s/blend_position"
@@ -18,10 +20,13 @@ const INPUT = {
 	RIGHT = "ui_right",
 	UP ="ui_up",
 	DOWN = "ui_down",
+	ROLL = "ui_roll",
 	ATTACK = "ui_attack"
 }
-const MAX_SPEED: int = 75
-const ACCELERATION: int = 15
+const DEFAULT_MAX_SPEED: int = 75
+const ROLLING_MAX_SPEED: int = 100
+const DEFAULT_ACCELERATION: int = 15
+const ROLLING_ACCELERATION: int = 50
 const DEFAULT_FRICTION: int = 15
 const ATTACK_FRICTION: int = 5
 
@@ -31,29 +36,31 @@ var _velocity = Vector2.ZERO
 
 onready var _animation_tree = $AnimationTree
 onready var _animation_tree_playback = _animation_tree.get("parameters/playback")
+onready var _attack_cooldown_timer = $AttackCooldownTimer
 
 
 func _ready():
 	_animation_tree.active = true
 
 
-func _process(delta) -> void:
-	_update_direction()
+func _process(_delta) -> void:
+	# Do not allow player to change directions while rolling
+	if not _is_rolling():
+		_update_direction()
 	
-	var is_moving = _direction != Vector2.ZERO
-	var is_attacking = _get_is_attacking()
-	
-	_update_state(is_attacking, is_moving)
+	_update_state()
 
 
 func _physics_process(_delta) -> void:
 	match _state:
 		State.IDLE:
-			_player_idle()
+			_idle_state()
 		State.MOVE:
-			_player_move(_direction)
+			_move_state()
+		State.ROLL:
+			_roll_state()
 		State.ATTACK:
-			_player_attack()
+			_attack_state()
 
 
 func _update_direction() -> void:
@@ -65,43 +72,67 @@ func _update_direction() -> void:
 	_direction = direction.normalized()
 
 
-func _get_is_attacking() -> bool:
+func _is_moving() -> bool:
+	return _direction != Vector2.ZERO
+
+
+func _is_attacking() -> bool:
 	var is_currently_attacking = _state == State.ATTACK
 	
 	if is_currently_attacking:
 		return true
+	elif _attack_cooldown_timer.is_stopped() and Input.is_action_just_pressed(INPUT.ATTACK):
+		_attack_cooldown_timer.start()
+		return true
 	else:
-		return Input.is_action_just_pressed(INPUT.ATTACK)
+		return false
 
 
-func _update_state(is_attacking: bool, is_moving: bool) -> void:
-	if is_attacking:
+func _is_rolling() -> bool:
+	var is_currently_rolling = _state == State.ROLL
+	
+	if is_currently_rolling:
+		return true
+	else:
+		# Only allow roll if player is moving
+		return _is_moving() && Input.is_action_just_pressed(INPUT.ROLL)
+
+
+func _update_state() -> void:
+	if _is_rolling():
+		_state = State.ROLL
+	elif _is_attacking():
 		_state = State.ATTACK
-	elif is_moving:
+	elif _is_moving():
 		_state = State.MOVE
 	else:
 		_state = State.IDLE
 
 
-func _player_idle() -> void:
+func _idle_state() -> void:
 	_stop_moving()
 	_animation_tree_playback.travel(ANIMATION.IDLE)
 
 
-func _player_move(direction: Vector2) -> void:
-	_update_animation_blend_position(direction)
-	_move(direction)
+func _move_state() -> void:
+	_update_animation_blend_position()
+	_move()
 	_animation_tree_playback.travel(ANIMATION.MOVE)
 
 
-func _player_attack() -> void:
+func _roll_state() -> void:
+	_move(ROLLING_MAX_SPEED, ROLLING_ACCELERATION)
+	_animation_tree_playback.travel(ANIMATION.ROLL)
+
+
+func _attack_state() -> void:
 	_stop_moving(ATTACK_FRICTION)
 	_animation_tree_playback.travel(ANIMATION.ATTACK)
 
 
-# Increase velocity by ACCELERATION until MAX_SPEED
-func _move(direction: Vector2) -> void:
-	var new_velocity = _velocity.move_toward(direction * MAX_SPEED, ACCELERATION)
+# Increase velocity by acceleration until max_speed
+func _move(max_speed := DEFAULT_MAX_SPEED, acceleration := DEFAULT_ACCELERATION) -> void:
+	var new_velocity = _velocity.move_toward(_direction * max_speed, acceleration)
 	_velocity = move_and_slide(new_velocity)
 
 
@@ -111,10 +142,15 @@ func _stop_moving(friction := DEFAULT_FRICTION) -> void:
 	_velocity = move_and_slide(new_velocity)
 
 
-func _update_animation_blend_position(blend_position: Vector2) -> void:
-	_animation_tree.set(ANIMATION_BLEND_POSITION_PARAM % ANIMATION.IDLE, blend_position)
-	_animation_tree.set(ANIMATION_BLEND_POSITION_PARAM % ANIMATION.MOVE, blend_position)
-	_animation_tree.set(ANIMATION_BLEND_POSITION_PARAM % ANIMATION.ATTACK, blend_position)
+func _update_animation_blend_position() -> void:
+	_animation_tree.set(ANIMATION_BLEND_POSITION_PARAM % ANIMATION.IDLE, _direction)
+	_animation_tree.set(ANIMATION_BLEND_POSITION_PARAM % ANIMATION.MOVE, _direction)
+	_animation_tree.set(ANIMATION_BLEND_POSITION_PARAM % ANIMATION.ROLL, _direction)
+	_animation_tree.set(ANIMATION_BLEND_POSITION_PARAM % ANIMATION.ATTACK, _direction)
+
+
+func _roll_animation_finished() -> void:
+	_state = State.IDLE
 
 
 func _attack_animation_finished() -> void:
