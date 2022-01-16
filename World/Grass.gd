f655eca1b2a6f4291fd1d7fa58648fb70df017bd
Dayton Bobbitt <12210193+dayton-bobbitt@users.noreply.github.com>
2022-01-16 11:00:22 -0600

    Create grass scene with hurtbox and death animation

---
+++ World/Grass.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+
+const ANIMATION = {
+	DEATH = "death"
+}
+
+onready var _animation_player = $AnimationPlayer
+
+
+func _on_Hurtbox_area_entered(area):
+	_animation_player.play(ANIMATION.DEATH)
+
+
+func _on_AnimationPlayer_animation_finished(anim_name):
+	match anim_name:
+		ANIMATION.DEATH:
+			queue_free()
