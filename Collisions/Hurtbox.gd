27663915200cc1eaa26f0563bbf23ca41c205baa
Dayton Bobbitt <12210193+dayton-bobbitt@users.noreply.github.com>
2022-01-17 18:19:09 -0600

    Add hit effect to bat

--- Collisions/Hurtbox.gd
+++ Collisions/Hurtbox.gd
@@ -7,12 +7,15 @@ signal destroyed
 
 
 export var max_health: float
+export var HitEffect: PackedScene
 
 onready var _health = max_health
+onready var _collision_shape = $CollisionShape2D
 
 
 func _ready():
 	assert(max_health > 0)
+	assert(_collision_shape.shape != null)
 
 
 func _take_damage(hitbox: Hitbox) -> void:
@@ -22,9 +25,17 @@ func _take_damage(hitbox: Hitbox) -> void:
 		var attack_direction = hitbox.get_parent().global_position.direction_to(global_position)
 		var knockback = attack_direction * hitbox.knockback_strength
 		
+		_play_hit_effect()
 		emit_signal("hit", knockback)
 	else:
 		emit_signal("destroyed")
+
+
+func _play_hit_effect() -> void:
+	if HitEffect != null:
+		var hitEffect = HitEffect.instance()
+		hitEffect.global_position = global_position
+		get_tree().current_scene.add_child(hitEffect)
 
 
 func _on_Hurtbox_area_entered(area):
